ba5bdd2c2ebe1c10e2bbb6c0f86522095f75dc9c
Favna <jeroen.claassens@live.nl>
2017-11-26 20:58:12 +0100

    Implement warning system

---
+++ src/commands/moderation/listwarn.js
@@ -0,0 +1,84 @@
+/*
+ *   This file is part of Ribbon
+ *   Copyright (C) 2017-2018 Favna
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, version 3 of the License
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   Additional Terms 7.b and 7.c of GPLv3 apply to this file:
+ *       * Requiring preservation of specified reasonable legal notices or
+ *         author attributions in that material or in the Appropriate Legal
+ *         Notices displayed by works containing it.
+ *       * Prohibiting misrepresentation of the origin of that material,
+ *         or requiring that modified versions of such material be marked in
+ *         reasonable ways as different from the original version.
+ */
+
+const Discord = require('discord.js'),
+	commando = require('discord.js-commando'),
+	jsonfile = require('jsonfile'),
+	moment = require('moment'),
+	path = require('path');
+
+module.exports = class listwarnCommand extends commando.Command {
+	constructor (client) {
+		super(client, {
+			'name': 'listwarn',
+			'group': 'moderation',
+			'aliases': ['reqwarn', 'lw', 'rw'],
+			'memberName': 'listwarn',
+			'description': 'Lists the warning points given to a member',
+			'examples': ['listwarn {member}'],
+			'guildOnly': true,
+			'throttling': {
+				'usages': 2,
+				'duration': 30
+			},
+
+			'args': [
+				{
+					'key': 'member',
+					'prompt': 'Which member to warn?',
+					'type': 'member',
+					'label': 'member name or ID'
+				}
+			]
+		});
+	}
+
+	hasPermission (msg) {
+		return msg.member.hasPermission('ADMINISTRATOR');
+	}
+
+	run (msg, args) {
+		const listWarnsEmbed = new Discord.MessageEmbed();
+
+		jsonfile.readFile(path.join(__dirname, `data/${msg.guild.id}/warnlog.json`), 'utf8', (readErr, obj) => {
+			if (readErr) {
+				return msg.reply(`📘 No warnpoints log found for this server, it will be created the first time you use the \`${msg.guild.commandPrefix}warn\` command`);
+
+			}
+
+			if (!obj[args.member.id]) {
+				return msg.reply('⚠️ That user has no warning points yet');
+			}
+			listWarnsEmbed
+				.setColor('#FFFF00')
+				.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+				.setFooter(moment().format('MMM Do YYYY | HH:mm:ss'))
+				.setDescription(`**Member:** ${args.member.user.tag} (${args.member.id})\n` +
+                    `**Current Warning Points:** ${obj[args.member.id].points}`);
+
+			return msg.embed(listWarnsEmbed);
+		});
+	}
+};
---
+++ src/commands/moderation/warn.js
@@ -0,0 +1,154 @@
+/*
+ *   This file is part of Ribbon
+ *   Copyright (C) 2017-2018 Favna
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, version 3 of the License
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   Additional Terms 7.b and 7.c of GPLv3 apply to this file:
+ *       * Requiring preservation of specified reasonable legal notices or
+ *         author attributions in that material or in the Appropriate Legal
+ *         Notices displayed by works containing it.
+ *       * Prohibiting misrepresentation of the origin of that material,
+ *         or requiring that modified versions of such material be marked in
+ *         reasonable ways as different from the original version.
+ */
+
+const Discord = require('discord.js'),
+	commando = require('discord.js-commando'),
+	fs = require('fs'),
+	jsonfile = require('jsonfile'),
+	moment = require('moment'),
+	{oneLine} = require('common-tags'),
+	path = require('path');
+
+module.exports = class warnCommand extends commando.Command {
+	constructor (client) {
+		super(client, {
+			'name': 'warn',
+			'group': 'moderation',
+			'aliases': ['warning'],
+			'memberName': 'warn',
+			'description': 'Warn a member with a specified amount of points',
+			'examples': ['warn {member} {points} {reason}'],
+			'guildOnly': true,
+			'throttling': {
+				'usages': 2,
+				'duration': 30
+			},
+
+			'args': [
+				{
+					'key': 'member',
+					'prompt': 'Which member to warn?',
+					'type': 'member',
+					'label': 'member name or ID'
+				},
+				{
+					'key': 'points',
+					'prompt': 'What do you want me to announce?',
+					'type': 'integer',
+					'label': 'warn points to give'
+				},
+				{
+					'key': 'reason',
+					'prompt': 'What is the reason you want to warn the user?',
+					'type': 'string',
+					'label': 'reason for the warning',
+					'default': ''
+				}
+			]
+		});
+	}
+
+	hasPermission (msg) {
+		return msg.member.hasPermission('ADMINISTRATOR');
+	}
+
+	run (msg, args) {
+		const modLogs = msg.guild.channels.exists('name', 'mod-logs') ? msg.guild.channels.find('name', 'mod-logs') : null,
+			warnEmbed = new Discord.MessageEmbed();
+
+
+		let warnobj = {},
+			warnpoints = 0;
+
+		jsonfile.readFile(path.join(__dirname, `data/${msg.guild.id}/warnlog.json`), 'utf8', (readErr, obj) => {
+			if (readErr) {
+				msg.reply('📘 No warnpoints log found for this server, creating one and filling with the first warning data');
+
+				warnobj = {
+					[args.member.id]: {
+						'usertag': args.member.user.tag,
+						'points': args.points
+					}
+				};
+
+				fs.mkdirSync(path.join(__dirname, `data/${msg.guild.id}`));
+				jsonfile.writeFile(path.join(__dirname, `data/${msg.guild.id}/warnlog.json`), warnobj, {'flag': 'wx+'}, (writeNoFileErr) => {
+					if (writeNoFileErr) {
+						console.error(`Error in command: Warn\nServer: ${msg.guild.id} | ${msg.guild.name}\nError: ${writeNoFileErr}`); // eslint-disable-line no-console
+
+						return msg.reply(oneLine `⚠️ An error occured writing the warning to disc and the error has been logged on Favna\'s end.
+							You can contact my developer on his server. Use \`${msg.guild.commandPrefix}invite\` to get an invite to his server.`);
+					}
+
+					warnEmbed
+						.setColor('#FFFF00')
+						.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+						.setDescription(`**Member:** ${args.member.user.tag} (${args.member.id})\n` +
+							'**Action:** Warn\n' +
+							`**Previous Warning Points:** ${warnpoints}\n` +
+							`**Current Warning Points:** ${args.points}\n` +
+							`**Reason:** ${args.reason !== '' ? args.reason : 'No reason has been added by the moderator'}`)
+						.setFooter(moment().format('MMM Do YYYY | HH:mm:ss'));
+
+					return modLogs !== null
+						? modLogs.send({'embed': warnEmbed})
+						: msg.reply('📃 I can keep a log of warning points given if you create a channel named \'mod-logs\' and give me access to it');
+				});
+			} else {
+				if (!obj[args.member.id]) {
+					obj[args.member.id] = {
+						'usertag': args.member.user.tag,
+						'points': args.points
+					};
+				} else {
+					warnpoints = obj[args.member.id].points;
+					obj[args.member.id].points = warnpoints + args.points;
+				}
+
+				jsonfile.writeFile(path.join(__dirname, `data/${msg.guild.id}/warnlog.json`), obj, (writeFileErr) => {
+					if (writeFileErr) {
+						return msg.reply(oneLine `⚠️ An error occured writing the warning to disc and the error has been logged on Favna\'s end.
+						You can contact my developer on his server. Use \`${msg.guild.commandPrefix}invite\` to get an invite to his server.`);
+					}
+
+					warnEmbed
+						.setColor('#FFFF00')
+						.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+						.setDescription(`**Member:** ${args.member.user.tag} (${args.member.id})\n` +
+							'**Action:** Warn\n' +
+							`**Previous Warning Points:** ${warnpoints}\n` +
+							`**Current Warning Points:** ${warnpoints + args.points}\n` +
+							`**Reason:** ${args.reason !== '' ? args.reason : 'No reason has been added by the moderator'}`)
+						.setFooter(moment().format('MMM Do YYYY | HH:mm:ss'));
+
+					return modLogs !== null
+						? modLogs.send({'embed': warnEmbed})
+						: msg.reply(oneLine `📃 📃 I can keep a log of warning points given if you create a channel named 'mod-logs' and give me access to it. 
+						Alternatively use the ${msg.guild.commandPrefix}listwarn command to view the current warning points for a given member`);
+				});
+			}
+		});
+	}
+};
