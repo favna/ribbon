7f5a2c749bc9bf0b3fdae1ab985acb5b5b375f84
Favna <sharkie.jeroen@gmail.com>
2018-03-30 11:18:01 +0200

    cleanup google and image

--- src/commands/searches/image.js
+++ src/commands/searches/image.js
@@ -37,9 +37,8 @@
 const {MessageEmbed} = require('discord.js'),
 	cheerio = require('cheerio'),
 	commando = require('discord.js-commando'),
-	querystring = require('querystring'),
-	request = require('snekfetch'),
-	{deleteCommandMessages} = require('../../util.js'),
+	request = require('snekfetch'), 
+	{deleteCommandMessages} = require('../../util.js'), 
 	{googleapikey, imageEngineKey} = require('../../auth.json');
 
 module.exports = class imageCommand extends commando.Command {
@@ -61,28 +60,25 @@ module.exports = class imageCommand extends commando.Command {
 				{
 					'key': 'query',
 					'prompt': 'What do you want to find images of?',
-					'type': 'string'
+					'type': 'string',
+					'parse': p => p.replace(/(who|what|when|where) ?(was|is|were|are) ?/gi, '')
+						.split(' ')
+						.map(x => encodeURIComponent(x))
+						.join('+')
 				}
 			]
 		});
 	}
 
 	async run (msg, args) {
-		const embed = new MessageEmbed(),
-			query = args.query
-				.replace(/(who|what|when|where) ?(was|is|were|are) ?/gi, '')
-				.split(' ')
-				.map(x => encodeURIComponent(x))
-				.join('+'),
-			safe = msg.channel.nsfw ? 'medium' : 'off',
-			QUERY_PARAMS = { // eslint-disable-line sort-vars
-				'cx': imageEngineKey,
-				'key': googleapikey,
-				safe,
-				'searchType': 'image'
-			};
+		const embed = new MessageEmbed();
 
-		let res = await request.get(`https://www.googleapis.com/customsearch/v1?${querystring.stringify(QUERY_PARAMS)}&q=${encodeURI(query)}`);
+		let res = await request.get('https://www.googleapis.com/customsearch/v1')
+			.query('cx', imageEngineKey)
+			.query('key', googleapikey)
+			.query('safe', msg.guild ? msg.channel.nsfw ? 'off' : 'medium' : 'high') // eslint-disable-line no-nested-ternary
+			.query('searchType', 'image')
+			.query('q', args.query);
 
 		if (res && res.body.items) {
 			embed
@@ -96,7 +92,11 @@ module.exports = class imageCommand extends commando.Command {
 		}
 
 		if (!res) {
-			res = await request.get(`https://www.google.com/search?tbm=isch&gs_l=img&safe=${safe}&q=${encodeURI(query)}`);
+			res = await request.get('https://www.google.com/search')
+				.query('tbm', 'isch')
+				.query('gs_l', 'img')
+				.query('safe', msg.guild ? msg.channel.nsfw ? 'off' : 'medium' : 'high') // eslint-disable-line no-nested-ternary
+				.query('q', args.query);
 
 			const $ = cheerio.load(res.text),
 				result = $('.images_table').find('img')
