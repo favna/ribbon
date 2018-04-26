3b93cce7c7afe8b41f6091595b62251f52b074fd
Favna <jeroen.claassens@live.nl>
2018-04-26 19:44:25 +0200

    remove todo

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -80,12 +80,6 @@ module.exports = class PlaySongCommand extends Command {
   }
 
   /* eslint-disable max-statements*/
-
-  /**
-   * @todo Reimplement Music Playlist support
-   * @body No guarantees here but I will try...
-   */
-
   async run (msg, args) {
     startTyping(msg);
     const url = args.url.replace(/<(.+)>/g, '$1'),
