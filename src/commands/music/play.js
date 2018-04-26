3a5d717568cbb707e67adbbc27a895299f348c77
Favna <sharkie.jeroen@gmail.com>
2018-04-26 12:35:50 +0200

    add todo in music

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -81,6 +81,11 @@ module.exports = class PlaySongCommand extends Command {
 
   /* eslint-disable max-statements*/
 
+  /**
+   * @todo Reimplement Music Playlist support
+   * @body No guarantees here but I will try...
+   */
+
   async run (msg, args) {
     startTyping(msg);
     const url = args.url.replace(/<(.+)>/g, '$1'),
