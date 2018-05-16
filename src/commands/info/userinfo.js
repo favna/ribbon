396366d7c35c8737bf81343db74b1227d6bfd7ec
Jeroen Claassens <jeroen.claassens@live.nl>
2018-05-16 11:25:01 +0200

    cleanup userinfo

--- src/commands/info/userinfo.js
+++ src/commands/info/userinfo.js
@@ -64,38 +64,32 @@ module.exports = class UserInfoCommand extends Command {
     });
   }
 
-  run (msg, args) {
+  run (msg, {member}) {
     startTyping(msg);
 
-    const uinfoEmbed = new MessageEmbed(),
-      vals = {
-        member: args.member,
-        user: args.member.user
-      };
+    const uinfoEmbed = new MessageEmbed();
 
     uinfoEmbed
-      .setAuthor(vals.user.tag)
-      .setThumbnail(vals.user.displayAvatarURL())
-      .setColor(vals.member.displayHexColor)
-      .addField('ID', vals.user.id, true)
-      .addField('Name', vals.user.username, true)
-      .addField('Nickname', vals.member.nickname ? vals.member.nickname : 'No Nickname', true)
-      .addField('Status', vals.user.presence.status !== 'dnd' ? capitalizeFirstLetter(vals.user.presence.status) : 'Do Not Disturb', true)
-      .addField(vals.user.presence.activity !== null
-        ? capitalizeFirstLetter(vals.user.presence.activity.type)
-        : 'Activity', vals.user.presence.activity !== null ? vals.user.presence.activity.name : 'Nothing', true)
-      .addField('Display Color', vals.member.displayHexColor, true)
-      .addField('Role(s)', vals.member.roles.size > 1 ? arrayClean(null, vals.member.roles.map((r) => {
+      .setAuthor(member.user.tag)
+      .setThumbnail(member.user.displayAvatarURL())
+      .setColor(member.displayHexColor)
+      .addField('ID', member.id, true)
+      .addField('Name', member.user.username, true)
+      .addField('Nickname', member.nickname ? member.nickname : 'No Nickname', true)
+      .addField('Status', member.user.presence.status !== 'dnd' ? capitalizeFirstLetter(member.user.presence.status) : 'Do Not Disturb', true)
+      .addField(member.user.presence.activity ? capitalizeFirstLetter(member.user.presence.activity.type) : 'Activity',
+        member.user.presence.activity ? member.user.presence.activity.name : 'Nothing', true)
+      .addField('Display Color', member.displayHexColor, true)
+      .addField('Role(s)', member.roles.size > 1 ? arrayClean(null, member.roles.map((r) => {
         if (r.name !== '@everyone') {
           return r.name;
         }
 
         return null;
       })).join(' | ') : 'None', false)
-      .addField('Account created at', moment(vals.user.createdAt).format('MMMM Do YYYY [at] HH:mm:ss [UTC]Z'), true)
-      .addField('Joined server at', moment(vals.member.joinedAt).format('MMMM Do YYYY [at] HH:mm:ss [UTC]Z'), true);
-    vals.member.roles.size >= 1 ? uinfoEmbed.setFooter(`${vals.member.displayName} has ${vals.member.roles.size - 1} role(s)`) : uinfoEmbed.setFooter(`${vals.member.displayName} has 0 roles`);
-
+      .addField('Account created at', moment(member.user.createdAt).format('MMMM Do YYYY [at] HH:mm:ss [UTC]Z'), true)
+      .addField('Joined server at', moment(member.joinedAt).format('MMMM Do YYYY [at] HH:mm:ss [UTC]Z'), true);
+    member.roles.size >= 1 ? uinfoEmbed.setFooter(`${member.displayName} has ${member.roles.size - 1} role(s)`) : uinfoEmbed.setFooter(`${member.displayName} has 0 roles`);
 
     deleteCommandMessages(msg, this.client);
     stopTyping(msg);
