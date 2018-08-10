78397b4cee6b0f466c3a703b465e0ba6fcdea492
Jeroen Claassens <jeroen.claassens@live.nl>
2018-08-10 17:07:15 +0200

    (core) Give my alt owner perms

--- src/Ribbon.js
+++ src/Ribbon.js
@@ -17,7 +17,7 @@ class Ribbon {
     this.token = token;
     this.client = new Client({
       commandPrefix: '!',
-      owner: '112001393140723712',
+      owners: ['112001393140723712', '268792781713965056'],
       unknownCommandResponse: false,
       presence: {
         status: 'online',
