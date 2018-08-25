4e88204c438494ba8c91e5a4060630418eb424ce
Jeroen Claassens <jeroen.claassens@live.nl>
2018-08-26 01:54:59 +0200

    Fix Announcement

--- src/commands/moderation/announce.js
+++ src/commands/moderation/announce.js
@@ -9,8 +9,10 @@
  * @returns {Message} Announcement you wrote in the announcement / news channel
  */
 
-const {Command} = require('discord.js-commando'), 
-  {stripIndents} = require('common-tags'), 
+const moment = require('moment'),
+  {Command} = require('discord.js-commando'), 
+  {MessageEmbed} = require('discord.js'),
+  {oneLine, stripIndents} = require('common-tags'), 
   {deleteCommandMessages, stopTyping, startTyping} = require('../../components/util.js');
 
 module.exports = class NewsCommand extends Command {
@@ -40,15 +42,17 @@ module.exports = class NewsCommand extends Command {
   }
 
   run (msg, {body}) {
-    startTyping(msg);
-    if (msg.guild.channels.find(c => c.name === 'announcements' || c.name === 'news')) {
-      const newsChannel = msg.guild.channels.find(c => c.name === 'announcements') ? msg.guild.channels.find(c => c.name === 'announcements') : msg.guild.channels.find(c => c.name === 'news');
+    try {
+      startTyping(msg);
 
-      if (newsChannel.permissionsFor(msg.guild.me).has(['SEND_MESSAGES', 'VIEW_CHANNEL'])) {
-        stopTyping(msg);
+      const announceEmbed = new MessageEmbed(),
+        newsChannel = msg.guild.channels.find(c => c.name === 'announcements') ? msg.guild.channels.find(c => c.name === 'announcements') : msg.guild.channels.find(c => c.name === 'news'),
+        modlogChannel = msg.guild.settings.get('modlogchannel',
+          msg.guild.channels.find(c => c.name === 'mod-logs') ? msg.guild.channels.find(c => c.name === 'mod-logs').id : null);
 
-        return msg.reply('I do not have permission to send messages to that channel. Better go and fix that!');
-      }
+      if (!newsChannel) throw new Error('nochannel');
+      if (!newsChannel.permissionsFor(msg.guild.me).has(['SEND_MESSAGES', 'VIEW_CHANNEL'])) throw new Error('noperms');
+
       newsChannel.startTyping(1);
 
       let announce = body;
@@ -56,20 +60,50 @@ module.exports = class NewsCommand extends Command {
       announce.slice(0, 4) !== 'http' ? announce = `${body.slice(0, 1).toUpperCase()}${body.slice(1)}` : null;
       msg.attachments.first() && msg.attachments.first().url ? announce += `\n${msg.attachments.first().url}` : null;
 
+      announceEmbed
+        .setColor('#AAEFE6')
+        .setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+        .setDescription(stripIndents`**Action:** Made an announcement`)
+        .setTimestamp();
+
+      newsChannel.send(announce);
+      newsChannel.stopTyping(true);
+
+      if (msg.guild.settings.get('modlogs', true)) {
+        if (!msg.guild.settings.get('hasSentModLogMessage', false)) {
+          msg.reply(oneLine`📃 I can keep a log of moderator actions if you create a channel named \'mod-logs\'
+                        (or some other name configured by the ${msg.guild.commandPrefix}setmodlogs command) and give me access to it.
+                        This message will only show up this one time and never again after this so if you desire to set up mod logs make sure to do so now.`);
+          msg.guild.settings.set('hasSentModLogMessage', true);
+        }
+        modlogChannel ? msg.guild.channels.get(modlogChannel).send('', {embed: announceEmbed}) : null;
+      }
+
       deleteCommandMessages(msg, this.client);
       stopTyping(msg);
-      newsChannel.stopTyping(true);
-      msg.say('Announcement has been made!');
       
-      return newsChannel.send(announce);
-    }
-    deleteCommandMessages(msg, this.client);
-    stopTyping(msg);
+      return msg.embed(announceEmbed);
+      
+    } catch (err) {
+      stopTyping(msg);
 
-    return msg.reply(stripIndents`To use the announce command you need a channel named \'news\' or \'announcements\'.
-    Here is a backup of your message:
-    \`\`\`
-    ${body}
-    \`\`\``);
+      if ((/(?:nochannel)/i).test(err.toString())) {
+        return msg.reply('there is no channel for me to make the announcement in. Create channel named either `announcements` or `news`');
+      } else if ((/(?:noperms)/i).test(err.toString())) {
+        return msg.reply('I do not have permission to send messages to the `announcements` or `news` channel. Better go fix that!'); 
+      }
+
+      this.client.channels.resolve(process.env.ribbonlogchannel).send(stripIndents`
+      <@${this.client.owners[0].id}> Error occurred in \`warn\` command!
+      **Server:** ${msg.guild.name} (${msg.guild.id})
+      **Author:** ${msg.author.tag} (${msg.author.id})
+      **Time:** ${moment(msg.createdTimestamp).format('MMMM Do YYYY [at] HH:mm:ss [UTC]Z')}
+      **Input:** ${body}
+      **Error Message:** ${err}
+      `);
+
+      return msg.reply(oneLine`An error occurred but I notified ${this.client.owners[0].username}
+      Want to know more about the error? Join the support server by getting an invite by using the \`${msg.guild.commandPrefix}invite\` command `);
+    }
   }
 };
