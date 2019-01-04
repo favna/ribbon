e5ef0974e32bdfa012889028022d312f79d68214
Jeroen Claassens <jeroen.claassens@live.nl>
2019-01-04 01:01:34 +0100

    Create API based doc generation for gulp
    This means that the only NPM scripts left are starting the bot coz they
    don't seem to work nicely with gulp in that they do not output to the
    console. Must say... gulp is quite nice when you get used to it.
    
    Signed-off-by: Jeroen Claassens <jeroen.claassens@live.nl>

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-const colors = require('colors');
+const fs = require('fs');
 const gulp = require('gulp');
 const gulpLint = require('gulp-tslint');
 const gulpTs = require('gulp-typescript');
+const jsdoc = require('jsdoc-to-markdown');
 const tslint = require('tslint');
 const ts = require('typescript');
 const uglify = require('uglify-es');
@@ -10,36 +11,39 @@ const mocha = require('gulp-mocha');
 const replace = require('gulp-string-replace');
 const gulpUglifyCompose = require('gulp-uglify/composer');
 const { argv } = require('yargs');
-const { exec } = require('child_process');
 
 const tsSource = ['./src/**/*.ts', './src/commands/**/*.ts'];
 const copySource = ['./src/data/fonts/*', './src/data/databases/*', './src/data/dex/*.json'];
+const jsdocOptions = {
+    template: fs.readFileSync('./docs/template.hbs', 'utf8'),
+    files: './dist/commands/*/*.js',
+    'example-lang': 'sh',
+};
 const replaceOptions = { logs: { enabled: false } };
 const minifier = gulpUglifyCompose(uglify, console);
 
-const replaceInSource = () => {
+const makeJavaScriptRunnable = () => {
     return gulp.src('./dist/Ribbon.js', { base: './dist' })
         .pipe(replace('typescript: true,', 'typescript: false,', replaceOptions))
         .pipe(replace(/.registerCommandsIn\({\n\s+dirname.+\n\s+filter.+\n\s+}\);/g, '.registerCommandsIn(path.join(__dirname, \'commands\'));', replaceOptions))
         .pipe(gulp.dest('./dist'));
 };
 
-const copySourceFiles = () => {
+const copyAdditionalFiles = () => {
     return gulp.src(copySource, { base: './src' })
         .pipe(gulp.dest('./dist/'));
 };
 
-const minify = () => {
+const minifyCode = () => {
     return gulp.src('./dist/**/*.js')
         .pipe(minifier())
         .pipe(gulp.dest('./dist'));
 };
 
-const docgen = () => {
-    return exec('yarn docs', (err, stdout, stderr) => {
-        console.log(colors.green(stdout));
-        console.log(colors.red(stderr));
-    });
+const generateDocs = () => {
+    const docs = jsdoc.renderSync(jsdocOptions);
+    fs.writeFileSync('./docs/docs.md', docs);
+    return fs.writeFileSync('../wikiribbon/All-Commands.md', docs);
 };
 
 const compileForDocs = () => {
@@ -50,7 +54,7 @@ const compileForDocs = () => {
         .js.pipe(gulp.dest('./dist'));
 };
 
-const compile = () => {
+const compileToJavaScript = () => {
     const tsProject = gulpTs.createProject('./tsconfig.json');
 
     return tsProject.src()
@@ -77,7 +81,7 @@ gulp.task('lint', () => {
 });
 
 gulp.task('clean', () => {
-    return del(['./dist', './docs/source']);
+    return del(['./dist']);
 });
 
 gulp.task('test', () => {
@@ -94,6 +98,6 @@ gulp.task('watch', () => {
     gulp.watch(tsSource, gulp.series('lint'));
 });
 
-gulp.task('docs', gulp.series(compileForDocs, docgen, 'clean'));
-gulp.task('build', gulp.series('clean', compile, gulp.parallel(copySourceFiles, replaceInSource), minify));
+gulp.task('docs', gulp.series('clean', compileForDocs, generateDocs, 'clean'));
+gulp.task('build', gulp.series('clean', compileToJavaScript, gulp.parallel(copyAdditionalFiles, makeJavaScriptRunnable), minifyCode));
 gulp.task('default', gulp.parallel('watch', 'lint'));
