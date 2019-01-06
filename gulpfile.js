a9d1b9aeba8753697062e7d5fe5afeef67dcabdc
Jeroen Claassens <jeroen.claassens@live.nl>
2019-01-06 17:09:14 +0100

    Build JSON docs through gulp too

--- gulpfile.js
+++ gulpfile.js
@@ -42,10 +42,10 @@ const minifyCode = () => {
 
 const generateDocs = (done) => {
     const docs = jsdoc.renderSync(jsdocOptions);
-    // const docsJSON = jsdoc.renderSync({json: true, ...jsdocOptions});
+    const docsJSON = jsdoc.getJsdocDataSync({files: jsdocOptions.files})
     fs.writeFileSync('./docs/index.md', docs);
     fs.writeFileSync('../wikiribbon/All-Commands.md', docs);
-    // fs.writeFileSync('../homesite/src/assets/docs/ribbon.json', docsJSON);
+    fs.writeFileSync('../homesite/src/assets/docs/ribbon.json', JSON.stringify(docsJSON));
     return done();
 };
 
@@ -128,7 +128,6 @@ gulp.task('watch', () => {
 
 gulp.task('docs', gulp.series('clean', compileForDocs, generateDocs, 'clean'));
 gulp.task('build', gulp.series('clean', compileToJavaScript, gulp.parallel(copyAdditionalFiles, makeJavaScriptRunnable), minifyCode));
-gulp.task('build:docs', gulp.series('clean', compileForDocs));
 gulp.task('rebuild', gulp.series(compileSingleToJavaScript));
 gulp.task('reload', gulp.series('rebuild'));
 gulp.task('default', gulp.parallel('watch', 'lint'));
