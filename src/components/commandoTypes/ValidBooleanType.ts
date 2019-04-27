5241f7ff1bfb85c6d9436f7f53fdebef174e6911
Jeroen Claassens <jeroen.claassens@live.nl>
2019-04-27 21:12:31 +0200

    Fix boolean arg validation

--- src/components/commandoTypes/ValidBooleanType.ts
+++ src/components/commandoTypes/ValidBooleanType.ts
@@ -1,22 +1,30 @@
 import { ArgumentType, CommandoClient } from 'awesome-commando';
 import { stripIndents } from 'common-tags';
-import { validBooleansMap } from '../Constants';
 
 export default class ValidBooleanType extends ArgumentType {
+    private readonly truthy: Set<string>;
+    private readonly falsy: Set<string>;
+
     constructor (client: CommandoClient) {
         super(client, 'validboolean');
+        this.truthy = new Set(['true', 't', 'yes', 'y', 'on', 'enable', 'enabled', '1', '+']);
+        this.falsy = new Set(['false', 'f', 'no', 'n', 'off', 'disable', 'disabled', '0', '-']);
     }
 
     public validate (value: string) {
-        if (validBooleansMap.includes(value)) return true;
+        const lc = value.toLowerCase();
+        if (this.truthy.has(lc) || this.falsy.has(lc)) return true;
 
         return stripIndents`
-            Has to be one of ${validBooleansMap.map(val => `\`${val}\``).join(', ')}
+            Has to be one of ${[...this.truthy, ...this.falsy].map(val => `\`${val}\``).join(', ')}
             Respond with your new selection or
         `;
     }
 
     public parse (value: string) {
-        return validBooleansMap.includes(value);
+        const lc = value.toLowerCase();
+        if (this.truthy.has(lc)) return true;
+        if (this.falsy.has(lc)) return false;
+        throw new RangeError('Unknown boolean value.');
     }
 }
