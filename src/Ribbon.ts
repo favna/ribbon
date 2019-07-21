4a73f60973b4a602aab5bcd0d27e673d573ad691
Jeroen Claassens <jeroen.claassens@live.nl>
2019-07-21 09:37:41 +0200

    Performance tweaks

--- src/Ribbon.ts
+++ src/Ribbon.ts
@@ -35,6 +35,7 @@ export default class Ribbon {
     this.client = new CommandoClient({
       commandPrefix: '!',
       owner: [ '268792781713965056', '437280139353653249' ],
+      botIds: [ '512150391471996930', '512152952908152833' ],
       invite: 'discord.gg/sguypX8',
       presence: {
         status: 'online',
@@ -44,7 +45,16 @@ export default class Ribbon {
         },
       },
       typescript: true,
-      userid: [ '512150391471996930', '512152952908152833' ],
+      disabledEvents: [ 'CHANNEL_PINS_UPDATE', 'CHANNEL_UPDATE', 'GUILD_BAN_ADD',
+        'GUILD_BAN_REMOVE', 'GUILD_EMOJIS_UPDATE', 'GUILD_INTEGRATIONS_UPDATE',
+        'GUILD_MEMBER_UPDATE', 'GUILD_ROLE_CREATE', 'GUILD_ROLE_DELETE',
+        'GUILD_ROLE_UPDATE', 'MESSAGE_DELETE_BULK', 'MESSAGE_DELETE',
+        'TYPING_START', 'WEBHOOKS_UPDATE', 'MESSAGE_REACTION_REMOVE_ALL', 'USER_UPDATE'
+      ],
+      ws: { compress: true },
+      restTimeOffset: 800,
+      messageCacheLifetime: 10 * 60,
+      messageSweepInterval: 5 * 60,
     });
   }
 
