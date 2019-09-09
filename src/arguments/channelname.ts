9c007727495523045882014298e0b3329dc1b72b
Jeroen Claassens <support@favware.tech>
2019-09-10 00:07:21 +0200

    fix(channelname): parsing for many-results

--- src/arguments/channelname.ts
+++ src/arguments/channelname.ts
@@ -1,18 +1,22 @@
+import FuzzySearch from '@utils/FuzzySearch';
 import { GuildChannel } from 'discord.js';
 import { Argument, KlasaGuild, KlasaMessage, Possible } from 'klasa';
-import FuzzySearch from '@utils/FuzzySearch';
 
 const CHANNEL_REGEXP = Argument.regex.channel;
 
 export default class ChannelnameArgument extends Argument {
-  run(arg: string, possible: Possible, message: KlasaMessage, filter?: (entry: GuildChannel) => boolean): GuildChannel {
+  run(arg: string, possible: Possible, message: KlasaMessage): GuildChannel {
     if (!arg) throw message.language.get('RESOLVER_INVALID_CHANNELNAME', possible.name);
     if (!message.guild) return this.channel.run(arg, possible, message);
     const resChannel = this.resolveChannel(arg, message.guild);
     if (resChannel) return resChannel;
 
-    const result = new FuzzySearch(message.guild!.channels, [ 'name', 'id' ]).run(message, arg);
-    if (result && result.length) return result[0];
+    if (CHANNEL_REGEXP.test(arg)) arg = arg.replace(CHANNEL_REGEXP, '$1');
+
+    const results = new FuzzySearch(message.guild!.channels, [ 'name', 'id' ]).run(message, arg);
+
+    if (results && results.length) return results[0];
+    if (results.length >= 5) throw `Found multiple matches: ${results.map(result => `<@${result}>`).join(', ')}. Please be more specific`;
     throw message.language.get('RESOLVER_INVALID_CHANNELNAME', possible.name);
   }
 
